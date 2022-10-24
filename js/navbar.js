128c6e35ccdd293decadc84291ebaf2696e3f0ac
Łukasz Sadowski <Lukasz.sadowski2605@gmail.com>
2022-10-24 19:11:11 +0200

    almost added second page

--- js/navbar.js
+++ js/navbar.js
@@ -2,14 +2,19 @@
 const burgerIcon = document.querySelector(".container__nav__burger");
 const closeIcon = document.querySelector(".close-btn");
 const homeList = document.querySelector(".container__nav__list");
+const navbarMobileList = document.querySelectorAll(".container__nav__link");
 
+// Function which show hiden navigation
 const showNavbar = () => {
   homeList.classList.remove("hide");
 };
 
+// Function which hide our navigation
 const closeNavbar = () => {
   homeList.classList.add("hide");
 };
 
+// Listeners
 burgerIcon.addEventListener("click", showNavbar);
 closeIcon.addEventListener("click", closeNavbar);
+navbarMobileList.forEach((item) => item.addEventListener("click", closeNavbar));
